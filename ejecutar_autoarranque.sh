1b346636c0183b1bc03cb188607924c526c0214b
Manel <ea3eiz@ea3eiz.com>
2023-03-22 19:26:16 +0100

    1

---
+++ ejecutar_autoarranque.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sed -i "6c Exec=sh cerrar_autoarranque.sh" /home/orangepi/Desktop/Autoarranque.desktop
+sed -i "7c Icon=/home/orangepi/ORANGEPI/AUTOARRANQUE.png" /home/orangepi/Desktop/Autoarranque.desktop
+sed -i "10c Name[es_ES]=Cerrar AUTOARRANQUE" /home/orangepi/Desktop/Autoarranque.desktop
+
+sed -i "15c AUTOARRANQUE=ON" /home/orangepi/status.ini
+
+cd /home/orangepi/qt
+./qt_autoarranque
