9f2da262fe089010abd07aa11e66977cef7baf51
Manel <ea3eiz@ea3eiz.com>
2023-03-22 21:29:47 +0100

    Update ejecutar_autoarranque.sh

--- ejecutar_autoarranque.sh
+++ ejecutar_autoarranque.sh
@@ -7,3 +7,7 @@ sed -i "15c AUTOARRANQUE=ON" /home/orangepi/status.ini
 
 cd /home/orangepi/qt
 ./qt_autoarranque
+
+sed -i "6c Exec=sh ejecutar_autoarranque.sh" /home/orangepi/Desktop/Autoarranque.desktop
+sed -i "7c Icon=/home/orangepi/ORANGEPI/ICONO_AUTOARRANQUE_OFF.png" /home/orangepi/Desktop/Autoarranque.desktop
+sed -i "10c Name[es_ES]=Abrir Autoarranque" /home/orangepi/Desktop/Autoarranque.desktop
