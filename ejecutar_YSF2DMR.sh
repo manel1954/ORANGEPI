4fde999ec54083ca100a3d5ad63dc8f3547259c6
Manel <ea3eiz@ea3eiz.com>
2023-03-23 21:55:15 +0100

    Update ejecutar_YSF2DMR.sh

--- ejecutar_YSF2DMR.sh
+++ ejecutar_YSF2DMR.sh
@@ -10,6 +10,6 @@ sed -i "14c YSF2DMR=ON" /home/orangepi/status.ini
 cd /home/orangepi/YSF2DMR
 
 
-mate-terminal --geometry 101x8+1277+797 --title=MMDVMDMR2YSF -x ./YSF2DMR YSF2DMR.ini & 
+mate-terminal --geometry 101x8+1277+797 --title=YSF2DMR -x ./YSF2DMR YSF2DMR.ini & 
 cd /home/orangepi/MMDVMHost
-mate-terminal --geometry 101x9+1277+987 --title=YSFGATEWAY -x ./MMDVMFUSION MMDVMFUSION.ini
+mate-terminal --geometry 101x9+1277+987 --title=MMDVMFUSION -x ./MMDVMFUSION MMDVMFUSION.ini
