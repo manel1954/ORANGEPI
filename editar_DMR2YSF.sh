b7ccf1a1383f0fd358d35957f11e90ad8bfb3839
Manel <ea3eiz@ea3eiz.com>
2023-03-23 22:43:05 +0100

    Update editar_DMR2YSF.sh

--- editar_DMR2YSF.sh
+++ editar_DMR2YSF.sh
@@ -1162,7 +1162,7 @@ do
                           [sS]* ) echo ""
                           sudo geany /home/orangepi/DMR2YSF/TG-YSFList.txt &
                           mate-terminal --geometry=120x30 -x less /home/orangepi/YSFClients/YSFGateway/YSFHosts.txt
-                          exit;
+                          #exit;
                           break;;
                           [nN]* ) echo ""
                           break;;
