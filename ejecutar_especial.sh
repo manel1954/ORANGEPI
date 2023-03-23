3069c047fe60ad4606943f653f45db96d62630ee
Manel <ea3eiz@ea3eiz.com>
2023-03-23 19:52:00 +0100

    1

--- ejecutar_especial.sh
+++ ejecutar_especial.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sed -i "6c Exec=sh cerrar_especial.sh" /home/orangepi/Desktop/ESPECIAL.desktop
-sed -i "7c Icon=/home/orangepi/ORANGEPI/ICONO_ESPECIAL_ON.png" /home/orangepi/Desktop/ESPECIAL.desktop
-sed -i "10c Name[es_ES]=Cerrar ESPECIAL" /home/orangepi/Desktop/ESPECIAL.desktop
+sed -i "6c Exec=sh cerrar_especial.sh" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
+sed -i "7c Icon=/home/orangepi/ORANGEPI/ICONO_ESPECIAL_ON.png" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
+sed -i "10c Name[es_ES]=Cerrar ESPECIAL" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
 
 sed -i "10c MMDVMESPECIAL=ON" /home/orangepi/status.ini
 
@@ -18,6 +18,6 @@ sudo killall MMDVMESPECIAL
 cd /home/orangepi/ORANGEPI
 sed -i "10c MMDVMESPECIAL=OFF" /home/orangepi/status.ini
 
-sed -i "6c Exec=mate-terminal --geometry 104x16+1151+880 --title=ESPECIAL -x sh ejecutar_especial.sh" /home/orangepi/Desktop/ESPECIAL.desktop
-sed -i "7c Icon=/home/orangepi/ORANGEPI/ICONO_ESPECIAL_OFF.png" /home/orangepi/Desktop/ESPECIAL.desktop
-sed -i "10c Name[es_ES]=Abrir ESPECIAL" /home/orangepi/Desktop/ESPECIAL.desktop
+sed -i "6c Exec=mate-terminal --geometry 104x16+1151+880 --title=ESPECIAL -x sh ejecutar_especial.sh" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
+sed -i "7c Icon=/home/orangepi/ORANGEPI/ICONO_ESPECIAL_OFF.png" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
+sed -i "10c Name[es_ES]=Abrir ESPECIAL" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
