578663dd0b5a66aefb2c54f5c0f3b5b21b3d42bd
Manel <ea3eiz@ea3eiz.com>
2023-03-27 23:22:39 +0200

    Update actualizar_ysf_reflectores.sh

--- actualizar_ysf_reflectores.sh
+++ actualizar_ysf_reflectores.sh
@@ -1,7 +1,5 @@
 #!/bin/bash		        
-			#echo "\33[1;32m ACTUALIZANDO REFLECTORES YSF>>>>>"
-			#sleep 10
-			
+			echo "\33[1;32m ACTUALIZANDO REFLECTORES YSF>>>>>"	
 			cd /home/orangepi/YSFClients/YSFGateway
 			sudo rm YSFHosts.txt
 			sudo wget -O YSFHosts.txt http://register.ysfreflector.de/export_csv.php
