c021e21912e7c381e9c9c8c445118abcb3c48718
Manel <ea3eiz@ea3eiz.com>
2023-03-27 23:30:09 +0200

    Create actualizar_YSFClients.sh

---
+++ actualizar_YSFClients.sh
@@ -0,0 +1,10 @@
+#!/bin/bash			            
+                        echo ">>>>>>>>> ACTUALIZANDO YSFClients"
+                        cd /home/orangepi/
+                        sudo rm -R YSFClients
+                        sleep 3
+                        cd /home/orangepi/
+                        git clone http://github.com/g4klx/YSFClients
+                        cd /home/orangepi/YSFClients/YSFGateway
+                        make clean
+                        make
