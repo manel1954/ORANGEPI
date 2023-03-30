bb21f34f21686ceba851732736d3d78a749adf4b
Manel <ea3eiz@ea3eiz.com>
2023-03-30 22:43:26 +0200

    Update copiar_restaurar_COPIAR.sh

--- copiar_restaurar_COPIAR.sh
+++ copiar_restaurar_COPIAR.sh
@@ -66,6 +66,6 @@
             cd /home/orangepi/     
             tar -zcvf copia.tar.gz COPIA_SEGURIDAD
             mv  copia.tar.gz /home/orangepi/COPIA_SEGURIDAD
-            sleep 10           
-            sudo rm -v /home/orangepi/COPIA_SEGURIDAD/!('copia.tar.gz')
+                      
+            sudo rm -v /home/orangepi/COPIA_SEGURIDAD/!(copia.tar.gz)
                   
