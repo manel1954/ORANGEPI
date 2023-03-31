9951d40750ccb0adf107ec28ee98bde5bab99976
Manel <ea3eiz@ea3eiz.com>
2023-03-31 11:04:12 +0200

    Update ejecutar_ambe_server.sh

--- ejecutar_ambe_server.sh
+++ ejecutar_ambe_server.sh
@@ -4,7 +4,6 @@ sed -i "6c Exec=sh cerrar_ambe_server.sh" /home/orangepi/Desktop/Abrir_ambe_serv
 sed -i "7c Icon=/home/orangepi/ORANGEPI/AMBER_SERVER_ON.png" /home/orangepi/Desktop/Abrir_ambe_server.desktop
 sed -i "10c Name[es_ES]=Cerrar AMBE SERVER" /home/orangepi/Desktop/Abrir_ambe_server.desktop
 
-
 sed -i "11c AMBE_SERVER=ON" /home/orangepi/status.ini
 
 puerto_router=$(awk "NR==1" /home/orangepi/ambe_server.ini) 
@@ -14,12 +13,9 @@ cd /home/orangepi/AMBEServer
 sudo killall AMBEserver
 sleep 3
 mate-terminal --geometry 74x11+730+275 -x sudo ./AMBEserver -p $puerto_router -i $puerto_modem -s $baut_rate
-#sleep 5
+
 sed -i "6c Exec=sh ejecutar_ambe_server.sh" /home/orangepi/Desktop/Abrir_ambe_server.desktop
 sed -i "7c Icon=/home/orangepi/ORANGEPI/AMBE_SERVER.png" /home/orangepi/Desktop/Abrir_ambe_server.desktop
 sed -i "10c Name[es_ES]=Abrir AMBE SERVER" /home/orangepi/Desktop/Abrir_ambe_server.desktop
 
 sed -i "11c AMBE_SERVER=OFF" /home/orangepi/status.ini
-
-
-
