ac09ef7a73b1ca0e1ddb38d327d0e2212c309cd4
Manel <ea3eiz@ea3eiz.com>
2023-04-01 23:08:35 +0200

    1

--- emparejar_rfcomm1.sh
+++ emparejar_rfcomm1.sh
@@ -10,18 +10,17 @@
 						echo "\33[1;33m" #amarillo
 			            echo "PEGA LA MAC DE TU BLUETOOTH aquí y pulsa Enter"	          		            
 			            read mac
-					    #sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh actualizar_02_07_06.sh;sudo rfcomm bind /dev/rfcomm1 $mac'" /home/pi/.config/autostart/actualizar.desktop
                         sudo sed -i "3c sudo rfcomm bind /dev/rfcomm1 $mac" /home/orangepi/.local/bluetooth.sh
-                        clear
-						echo ""
-						echo ""
-						echo ""
-						echo "\33[1;31m" #color rojo
-						echo "********************************************************************"
-						echo "*    SE VA A REINICIAR EL SISTEMA PARA ANCLAR EL PUERTO rfcomm1    *"
-						echo "********************************************************************"
-						echo "\33[1;37m" #color
-						echo "Pulsa Enter para seguir"
-			            sleep 3
-						sudo reboot
+                        #clear
+						#echo ""
+						#echo ""
+						#echo ""
+						#echo "\33[1;31m" #color rojo
+						#echo "********************************************************************"
+						#echo "*    SE VA A REINICIAR EL SISTEMA PARA ANCLAR EL PUERTO rfcomm1    *"
+						#echo "********************************************************************"
+						#echo "\33[1;37m" #color
+						#echo "Pulsa Enter para seguir"
+			            #sleep 3
+						#sudo reboot
 
