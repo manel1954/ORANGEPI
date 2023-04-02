ec1feed3c1ca0553961d07fd4141912af68064b2
Manel <ea3eiz@ea3eiz.com>
2023-04-02 23:52:00 +0200

    Update cerrar_DMR2YSF.sh

--- cerrar_DMR2YSF.sh
+++ cerrar_DMR2YSF.sh
@@ -3,8 +3,8 @@ sed -i '6c Exec=sudo sh -c "cd /home/orangepi/ORANGEPI/;sh ejecutar_DMR2YSF.sh"'
 sed -i "7c Icon=/home/orangepi/ORANGEPI/DMR2YSF.png" /home/orangepi/Desktop/Abrir_DMR2YSF.desktop
 sed -i "10c Name[es_ES]=Abrir DMR2YSF" /home/orangepi/Desktop/Abrir_DMR2YSF.desktop
 
-#sed -i "14c DMR2YSF=OFF" /home/orangepi/status.ini
+sed -i "14c DMR2YSF=OFF" /home/orangepi/status.ini
 
-sudo killall MMDVMHost
+sudo killall MMDVMDMR2YSF
 sudo killall DMR2YSF
 sudo killall YSFGateway
