7d5c5decb40fc75388749d96ef272d7bb2bb7968
Manel <ea3eiz@ea3eiz.com>
2023-04-04 15:33:40 +0200

    1

--- ejecutar_bm_05.sh
+++ ejecutar_bm_05.sh
@@ -12,13 +12,13 @@ echo "***********************************************"
 echo "*                 BRANDMEISTER                * "
 echo "***********************************************"
 echo "\33[38;5;138m"
-sudo ./MMDVMBM MMDVMBM.ini
+mate-terminal --geometry 104x16+772+880 --title=BRANDMEISTER -x sudo ./MMDVMBM MMDVMBM.ini
 clear
 sudo killall MMDVMBM
 cd /home/orangepi/ORANGEPI
 sed -i "7c MMDVMBM=OFF" /home/orangepi/status.ini
 
-sed -i "6c Exec=mate-terminal --geometry 104x16+810+880 --title=BRANDMEISTER -x sh ejecutar_bm_05.sh" /home/orangepi/Desktop/BM.desktop
+sed -i "6c Exec=mate-terminal --geometry 104x16+772+880 --title=BRANDMEISTER -x sh ejecutar_bm_05.sh" /home/orangepi/Desktop/BM.desktop
 sed -i "7c Icon=/home/orangepi/ORANGEPI/DMR.png" /home/orangepi/Desktop/BM.desktop
 sed -i "10c Name[es_ES]=Abrir BM" /home/orangepi/Desktop/BM.desktop
 
