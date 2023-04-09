36575565f5744a7bcdabbb30941241f55733ef98
Manel <ea3eiz@ea3eiz.com>
2023-04-09 13:05:01 +0200

    Update ejecutar_svxlink_05.sh

--- ejecutar_svxlink_05.sh
+++ ejecutar_svxlink_05.sh
@@ -15,8 +15,8 @@ sed -i "4c Name[es_ES]=Cerrar SVXLINK" /home/orangepi/Desktop/Abrir_SVXLink.desk
 sed -i "8c SVXLINK=ON" /home/orangepi/status.ini
 
 echo "{$CIAN}"
-mate-terminal --geometry 67x30+22+50 --title=SVXLINK -x sudo svxlink
+mate-terminal --geometry 68x27+22+50 --title=SVXLINK -x sudo svxlink
 
-sed -i "6c Exec=mate-terminal --geometry 67x30+22+50 --title=SVXLINK -x sh ejecutar_svxlink_05.sh" /home/orangepi/Desktop/Abrir_SVXLink.desktop
+sed -i "6c Exec=mate-terminal --geometry 68x27+22+50 --title=SVXLINK -x sh ejecutar_svxlink_05.sh" /home/orangepi/Desktop/Abrir_SVXLink.desktop
 sed -i "7c Icon=/home/orangepi/ORANGEPI/DMRPLUS_EDITOR.png" /home/orangepi/Desktop/Abrir_SVXLink.desktop
 sed -i "4c Name[es_ES]=Abrir SVXLINK" /home/orangepi/Desktop/Abrir_SVXLink.desktop
